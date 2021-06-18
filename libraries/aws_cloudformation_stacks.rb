cc115c831a2736d4faafb14fdf3ff38cf4d2b17a
Sebastian Rosario <sebastian6417@gmail.com>
2021-06-18 15:45:25 -0400

    Update aws_cloudformation_stacks.rb

--- libraries/aws_cloudformation_stacks.rb
+++ libraries/aws_cloudformation_stacks.rb
@@ -50,7 +50,7 @@ class AwsCloudformationStacks < AwsResourceBase
           root_id: res.root_id,
           tags: res.tags,
         }]
-        puts cloudformation_stacks_rows.where(tags: /"lm_app" => "aws-gds-ds-chef-role"/)
+        puts cloudformation_stacks_rows.where(name: 'gds-ds-chef-role-non-production-ets')
       end
       break unless @api_response.next_token
       pagination_options = { next_token: @api_response.next_token }
