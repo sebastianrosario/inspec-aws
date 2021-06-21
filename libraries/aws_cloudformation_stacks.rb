9f0bb101ddaa59f4a4490106a7b35e7f80b26326
Rosario <n1556284@CVDE14X-TBSAN7P.lm.lmig.com>
2021-06-21 14:42:02 -0400

    Changed tags name

--- libraries/aws_cloudformation_stacks.rb
+++ libraries/aws_cloudformation_stacks.rb
@@ -40,7 +40,7 @@ class AwsCloudformationStacks < AwsResourceBase
       end
       return cloudformation_stacks_rows if !@api_response || @api_response.empty?
       @api_response.stacks.each do |res|
-        cf_tags = map_tags(res.tags)
+        # cf_tags = map_tags(res.tags)
         cloudformation_stacks_rows+=[{
           name: res.stack_name,
           creation_time: res.creation_time,
@@ -49,7 +49,7 @@ class AwsCloudformationStacks < AwsResourceBase
           role_arn: res.role_arn,
           parent_id: res.parent_id,
           root_id: res.root_id,
-          tags: cf_tags,
+          tags: map_tags(res.tags),
         }]
         
       end
