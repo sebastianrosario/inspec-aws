8eb6816a4ce06415023134e0d03a4906b52d1db3
Rosario <n1556284@CVDE14X-TBSAN7P.lm.lmig.com>
2021-06-21 15:17:50 -0400

    Changed tags name

--- libraries/aws_cloudformation_stacks.rb
+++ libraries/aws_cloudformation_stacks.rb
@@ -18,7 +18,7 @@ class AwsCloudformationStacks < AwsResourceBase
              .register_column(:names, field: :name)
              .register_column(:creation_times, field: :creation_time)
              .register_column(:stack_ids, field: :stack_id)
-             .register_column(:tags2, field: :tags2)
+             .register_column(:tags2, field: :tags)
              .register_column(:notification_arns, field: :notification_arn)
              .register_column(:role_arns, field: :role_arn)
              .register_column(:parent_ids, field: :parent_id)
