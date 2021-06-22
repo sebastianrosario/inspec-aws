18ca0c8c9a874178bc63beada059ff0c64e4bb7f
Rosario <n1556284@CVDE14X-TBSAN7P.lm.lmig.com>
2021-06-22 08:54:17 -0400

    Changed tags name

--- libraries/aws_cloudformation_stacks.rb
+++ libraries/aws_cloudformation_stacks.rb
@@ -31,7 +31,7 @@ class AwsCloudformationStacks < AwsResourceBase
     @table = fetch_data
   end
 
-  def map_tags2(tag_list)
+  def map_tags_test(tag_list)
     return {} if tag_list.nil? || tag_list.empty?
     tags = {}
     tag_list.each do |tag|
@@ -50,7 +50,7 @@ class AwsCloudformationStacks < AwsResourceBase
       end
       return cloudformation_stacks_rows if !@api_response || @api_response.empty?
       @api_response.stacks.each do |res|
-
+        cf_tags = map_tags(res.tags)
         cloudformation_stacks_rows+=[{
           name: res.stack_name,
           creation_time: res.creation_time,
@@ -59,7 +59,7 @@ class AwsCloudformationStacks < AwsResourceBase
           role_arn: res.role_arn,
           parent_id: res.parent_id,
           root_id: res.root_id,
-          tags: map_tags2(res.tags),
+          tags: cf_tags,
         }]
         
       end
